f40f2c1d17969bbba01c9fa6c9b2fbaed665a995
Virendra chaudhary <virendremoond@gmail.com>
2026-05-11 18:10:27 +0530

    remove obsolete scripts

--- fix.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/App.jsx', 'utf8');
-content = content.replace(/\\`/g, '`');
-content = content.replace(/\\\${/g, '${');
-content = content.replace(/\\\\n/g, '\\n');
-fs.writeFileSync('src/App.jsx', content);
-console.log("Fixed backslashes in App.jsx");
